509fdbc7694771c56db1ebf65b22c759e3620be9
m43c <marqencinas@gmail.com>
2023-09-29 11:07:45 -0400

    Add user login

--- server/src/controllers/auth.controler.js
+++ server/src/controllers/auth.controler.js
@@ -30,3 +30,34 @@ export const signup = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+export const signin = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const userFound = await User.findOne({ email });
+
+        if (!userFound) {
+            return res.status(400).json({ message: "User not found" });
+        }
+
+        const isMatch = await bcrypt.compare(password, userFound.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Incorrect password" });
+        }
+
+        const token = await createAccessToken({ id: userFound._id });
+
+        res.cookie("token", token);
+        res.json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+            createdAt: userFound.createdAt,
+            updatedAt: userFound.updatedAt,
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { signup } from "../controllers/auth.controler.js";
+import { signup, signin } from "../controllers/auth.controler.js";
 
 const router = Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
