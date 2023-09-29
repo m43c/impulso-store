fc11527b474ee58c4bd18680281bcd4db54969a6
m43c <marqencinas@gmail.com>
2023-09-29 14:45:22 -0400

    Add token validation

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
-import { signup, signin, logout } from "../controllers/auth.controler.js";
+import {
+    signup,
+    signin,
+    logout,
+    profile,
+} from "../controllers/auth.controler.js";
+import { authRequired } from "../middlewares/validateToken.js";
 
 const router = Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/logout", logout);
+router.get("/profile", authRequired, profile);
 
 export default router;
