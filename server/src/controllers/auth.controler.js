ebfad689f68879e0a879c48f3ce9191582fe01cc
m43c <marqencinas@gmail.com>
2023-09-28 22:30:58 -0400

    Add a token credential to logged in user

--- server/src/controllers/auth.controler.js
+++ server/src/controllers/auth.controler.js
@@ -1,18 +1,24 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { createAccessToken } from "../libs/jwt.js ";
 
 export const signup = async (req, res) => {
     const { username, email, password } = req.body;
 
     try {
         const passwordHash = await bcrypt.hash(password, 10);
+
         const newUser = new User({
             username,
             email,
             password: passwordHash,
         });
+
         const userSaved = await newUser.save();
 
+        const token = await createAccessToken({ id: userSaved._id });
+
+        res.cookie("token", token);
         res.json({
             id: userSaved._id,
             username: userSaved.username,
