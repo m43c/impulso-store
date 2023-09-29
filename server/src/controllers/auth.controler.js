42df94520c3bd5214544b703966f9a085ba43d0a
m43c <marqencinas@gmail.com>
2023-09-29 11:20:52 -0400

    Add logout to user

--- server/src/controllers/auth.controler.js
+++ server/src/controllers/auth.controler.js
@@ -61,3 +61,8 @@ export const signin = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+export const logout = (req, res) => {
+    res.cookie("token", "", { expires: new Date(0) });
+    return res.sendStatus(200);
+};
--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { signup, signin } from "../controllers/auth.controler.js";
+import { signup, signin, logout } from "../controllers/auth.controler.js";
 
 const router = Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.post("/logout", logout);
 
 export default router;
