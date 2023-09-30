72beecb9035a5534efb40c5dc503facbc6ba6b65
m43c <marqencinas@gmail.com>
2023-09-29 20:33:20 -0400

    Add data validation

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -6,11 +6,13 @@ import {
     profile,
 } from "../controllers/auth.controler.js";
 import { authRequired } from "../middlewares/validateToken.js";
+import { signupSchema, signinSchema } from "../schemas/auth.schema.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
 
 const router = Router();
 
-router.post("/signup", signup);
-router.post("/signin", signin);
+router.post("/signup", validateSchema(signupSchema), signup);
+router.post("/signin", validateSchema(signinSchema), signin);
 router.post("/logout", logout);
 router.get("/profile", authRequired, profile);
 
