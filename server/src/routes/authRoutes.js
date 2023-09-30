2a5893959b8ce29224318b41b0db121e080d75d6
m43c <marqencinas@gmail.com>
2023-09-29 22:27:47 -0400

    Fix file names

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -5,7 +5,7 @@ import {
     logout,
     profile,
 } from "../controllers/authController.js";
-import { validateToken } from "../middlewares/tokeValidation.js";
+import { validateToken } from "../middlewares/tokenValidation.js";
 import { signupSchema, signinSchema } from "../schemas/authSchema.js";
 import { validateSchema } from "../middlewares/schemaValidation.js";
 
