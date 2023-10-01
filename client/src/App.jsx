83f4833a21884f1445d4b284932cf251441112b2
m43c <marqencinas@gmail.com>
2023-10-01 11:03:19 -0400

    Add page routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-    return <h1>Hello world</h1>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<h1>Home Page</h1>} />
+                <Route path="/signup" element={<h1>Sign Up</h1>} />
+                <Route path="/signin" element={<h1>Sign In</h1>} />
+                <Route path="/logout" element={<h1>Logout</h1>} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
