f04d4ce81f95a1ed424e0a294418cd5cab3948b7
m43c <marqencinas@gmail.com>
2023-10-04 19:53:58 -0400

    Add role validation in product routes

---
+++ server/src/middlewares/roleValidation.js
@@ -0,0 +1,15 @@
+import User from "../models/userModel.js";
+import Role from "../models/roleModel.js";
+
+export const validateRole = async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+    const roles = await Role.find({ _id: { $in: user.roles } });
+
+    for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "admin") {
+            return next();
+        }
+    }
+
+    return res.status(403).json({ message: "Require admin role" });
+};
--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { validateToken } from "../middlewares/tokenValidation.js";
+import { validateRole } from "../middlewares/roleValidation.js";
 import {
     createProduct,
     readProducts,
@@ -10,10 +11,10 @@ import {
 
 const router = Router();
 
-router.post("/products", validateToken, createProduct);
+router.post("/products", validateToken, validateRole, createProduct);
 router.get("/products", readProducts);
 router.get("/products/:id", readProduct);
-router.put("/products/:id", validateToken, updateProduct);
-router.delete("/products/:id", validateToken, deleteProduct);
+router.put("/products/:id", validateToken, validateRole, updateProduct);
+router.delete("/products/:id", validateToken, validateRole, deleteProduct);
 
 export default router;
