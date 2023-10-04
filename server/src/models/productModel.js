da22169ded229b3ed91be2c627e3837b06e803bd
m43c <marqencinas@gmail.com>
2023-10-04 11:34:46 -0400

    Add product model

---
+++ server/src/models/productModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        imgURL: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        description: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        price: {
+            type: Number,
+            trim: true,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export default mongoose.model("Product", productSchema);
