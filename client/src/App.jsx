f2d575aa4a1a546c29ac7bf84fe96e664812e3f3
m43c <marqencinas@gmail.com>
2023-10-05 17:43:34 -0400

    Add styles to the registration page through CSS modules

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import ProtectedRoute from "./ProtectedRoute";
+import HomePage from "./pages/HomePage";
 import RegistrationPage from "./pages/RegistrationPage";
 import LoginPage from "./pages/LoginPage";
-import HomePage from "./pages/HomePage";
+import Footer from "./components/Footer";
 
 function App() {
     return (
         <AuthProvider>
             <BrowserRouter>
+                <Navbar />
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/signup" element={<RegistrationPage />} />
@@ -17,6 +20,7 @@ function App() {
                     <Route element={<ProtectedRoute />}></Route>
                 </Routes>
             </BrowserRouter>
+            <Footer />
         </AuthProvider>
     );
 }
