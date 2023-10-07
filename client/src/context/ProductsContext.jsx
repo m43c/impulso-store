886106b484df80ebca68cb683e7158aaf4ade72b
m43c <marqencinas@gmail.com>
2023-10-06 23:11:36 -0400

    Add create product function

--- client/src/context/ProductsContext.jsx
+++ client/src/context/ProductsContext.jsx
@@ -1,9 +1,10 @@
 import { createContext, useContext, useState } from "react";
+import { createProductRequest } from "../api/products";
 
 export const ProductContext = createContext();
 
 export const useProducts = () => {
-    const context = useContext(AuthContext);
+    const context = useContext(ProductContext);
 
     if (!context) {
         throw new Error("useProducts must be used within an ProductProvider");
@@ -15,8 +16,13 @@ export const useProducts = () => {
 export const ProductProvider = ({ children }) => {
     const [products, setProducts] = useState([]);
 
+    const createProduct = async (product) => {
+        const res = await createProductRequest(product);
+        return console.log(res);
+    };
+
     return (
-        <ProductContext.Provider value={{ products }}>
+        <ProductContext.Provider value={{ products, createProduct }}>
             {children}
         </ProductContext.Provider>
     );
