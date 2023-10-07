3be8069a90fd359d318c28828e7883061cd33d17
m43c <marqencinas@gmail.com>
2023-10-07 18:28:41 -0400

    Add style to the product form

--- client/src/pages/ProductFormPage.jsx
+++ client/src/pages/ProductFormPage.jsx
@@ -1,5 +1,7 @@
 import { useForm } from "react-hook-form";
 import { useProducts } from "../context/ProductsContext";
+import { Link } from "react-router-dom";
+import styles from "./ProductFormPage.module.css";
 
 function ProductFormPage() {
     const { register, handleSubmit } = useForm();
@@ -10,29 +12,60 @@ function ProductFormPage() {
     });
 
     return (
-        <div>
-            <form onSubmit={onSubmit}>
+        <div className={styles.mainContainer}>
+            <h1 className={styles.mainTitle}>New Product</h1>
+
+            <form className={styles.formContainer} onSubmit={onSubmit}>
                 <input
                     type="text"
                     placeholder="Name"
+                    className={styles.formInput}
                     autoFocus
                     {...register("name")}
                 />
 
                 <input
                     type="text"
-                    placeholder="Image"
+                    placeholder="Image URL"
+                    className={styles.formInput}
                     {...register("image")}
                 />
 
                 <textarea
                     rows="3"
                     placeholder="Description"
+                    className={`${styles.formInput} ${styles.formInputTextarea}`}
                     {...register("description")}
                 ></textarea>
-                <input type="text" placeholder="Price" {...register("price")} />
 
-                <button>Save</button>
+                <input
+                    type="text"
+                    placeholder="Price"
+                    className={styles.formInput}
+                    {...register("price")}
+                />
+
+                <div className={styles.formButtons}>
+                    <Link
+                        className={`${styles.formBtn} ${styles.formBtnCancel}`}
+                        to="/products"
+                    >
+                        Cancel
+                    </Link>
+
+                    <button
+                        type="reset"
+                        className={`${styles.formBtn} ${styles.formBtnReset}`}
+                    >
+                        Reset
+                    </button>
+
+                    <button
+                        className={`${styles.formBtn} ${styles.formBtnSave}`}
+                    >
+                        Save
+                    </button>
+                </div>
             </form>
         </div>
     );
