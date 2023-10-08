bcfca3d3bfb888c088b1ab8f77b79a7749e17aea
m43c <marqencinas@gmail.com>
2023-10-08 17:55:53 -0400

    Add styles to the font

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
+import styles from "./HomePage.module.css";
+
 function HomePage() {
-    return <h1>HomePage</h1>;
+    return <h1 className={styles.title}>Home Page</h1>;
 }
 
 export default HomePage;
