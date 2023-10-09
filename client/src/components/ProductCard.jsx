f56a5370cb6f0fba7b327e97cba10ae47209b483
m43c <marqencinas@gmail.com>
2023-10-08 22:41:29 -0400

    Add product information and delete product button

---
+++ client/src/components/ProductCard.jsx
@@ -0,0 +1,30 @@
+import { useProducts } from "../context/ProductsContext";
+import styles from "./ProductCard.module.css";
+
+function ProductCard({ product }) {
+    const { deleteProduct } = useProducts();
+
+    return (
+        <div key={product._id} className={styles.product}>
+            <img src={product.image} className={styles.image} />
+            <h1 className={styles.name}>{product.name}</h1>
+            <p className={styles.description}>{product.description}</p>
+            <span className={styles.price}>{`${product.price}$`}</span>
+            <div className={styles.buttons}>
+                <button
+                    className={`${styles.button} ${styles.deleteBtn}`}
+                    onClick={() => {
+                        deleteProduct(product._id);
+                    }}
+                >
+                    Delete
+                </button>
+                <button className={`${styles.button} ${styles.editBtn}`}>
+                    Edit
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default ProductCard;
