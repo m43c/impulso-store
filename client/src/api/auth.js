7a65fe21da9715d2ef0c3eb570c76524b7357d13
m43c <marqencinas@gmail.com>
2023-10-09 20:26:00 -0400

    Add profileRequest function

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -2,4 +2,5 @@ import axios from "./axios";
 
 export const signupRequest = (user) => axios.post("/signup", user);
 export const signinRequest = (user) => axios.post("/signin", user);
+export const profileRequest = () => axios.get("/profile");
 export const verifyTokenRequest = () => axios.get("/verify");
