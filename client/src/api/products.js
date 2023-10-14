f80286d646b86b0a6fcfaba1c4e036f02fc70fcf
m43c <marqencinas@gmail.com>
2023-10-13 20:30:14 -0400

    Update route names

--- client/src/api/products.js
+++ client/src/api/products.js
@@ -1,13 +1,13 @@
 import axios from "./axios";
 
 export const createProductRequest = (product) =>
-    axios.post("/products", product);
+    axios.post("/add-product", product);
 
 export const readProductsRequest = () => axios.get("/products");
 
-export const readProductRequest = (id) => axios.get(`/products/${id}`);
+export const readProductRequest = (id) => axios.get(`/product/${id}`);
 
 export const updateProductRequest = (id, product) =>
-    axios.put(`/products/${id}`, product);
+    axios.put(`/product/${id}`, product);
 
-export const deleteProductRequest = (id) => axios.delete(`/products/${id}`);
+export const deleteProductRequest = (id) => axios.delete(`/product/${id}`);
