3e4db425e6bafaaf2c2949e9178a0aeb0373f6ad
m43c <marqencinas@gmail.com>
2023-10-13 21:38:06 -0400

    Add products view for current user

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,9 +1,16 @@
 import { useProducts } from "../context/ProductsContext";
+import { useAuth } from "../context/AuthContext";
 import { Link } from "react-router-dom";
 import styles from "./ProductCard.module.css";
+import { useEffect } from "react";
 
 function ProductCard({ product }) {
     const { deleteProduct } = useProducts();
+    const { profile, user } = useAuth();
+
+    useEffect(() => {
+        profile();
+    }, []);
 
     return (
         <article key={product._id} className={styles.product}>
@@ -12,20 +19,26 @@ function ProductCard({ product }) {
             <p className={styles.description}>{product.description}</p>
             <span className={styles.price}>{`${product.price}$`}</span>
             <div className={styles.buttons}>
-                <button
-                    className={`${styles.button} ${styles.deleteBtn}`}
-                    onClick={() => {
-                        deleteProduct(product._id);
-                    }}
-                >
-                    Delete
-                </button>
-                <Link
-                    className={`${styles.button} ${styles.editBtn}`}
-                    to={`/add-product/${product._id}`}
-                >
-                    Edit
-                </Link>
+                {user.roles[0].name === "admin" ? (
+                    <>
+                        <button
+                            className={`${styles.button} ${styles.deleteBtn}`}
+                            onClick={() => {
+                                deleteProduct(product._id);
+                            }}
+                        >
+                            Delete
+                        </button>
+                        <Link
+                            className={`${styles.button} ${styles.editBtn}`}
+                            to={`/add-product/${product._id}`}
+                        >
+                            Edit
+                        </Link>
+                    </>
+                ) : (
+                    <></>
+                )}
             </div>
         </article>
     );
