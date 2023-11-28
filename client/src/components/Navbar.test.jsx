85f734f97ada4f0633cfe9bbafc281d28ca13c26
m43c <marqencinas@gmail.com>
2023-11-27 23:00:58 -0400

    Add test for menu state

---
+++ client/src/components/Navbar.test.jsx
@@ -0,0 +1,44 @@
+import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "../context/AuthContext";
+import { ProductProvider } from "../context/ProductsContext";
+
+import Navbar from "./Navbar";
+
+describe("Navbar", () => {
+  test("should toggle the menu visibility when the button is clicked", () => {
+    const { rerender } = render(
+      <AuthProvider>
+        <ProductProvider>
+          <BrowserRouter>
+            <Navbar />
+          </BrowserRouter>
+        </ProductProvider>
+      </AuthProvider>
+    );
+
+    // Get menu element and verify it starts out hidden  
+    let menu = screen.getByRole("navigation");
+    expect(menu.classList.contains("translate-x-full")).toBe(true);
+
+    // Click on the menu toggle button  
+    const button = screen.getByRole("button");
+    button.click();
+
+    // Re-render component to update internal state
+    rerender(
+      <AuthProvider>
+        <ProductProvider>
+          <BrowserRouter>
+            <Navbar />
+          </BrowserRouter>
+        </ProductProvider>
+      </AuthProvider>
+    );
+
+    // Now menu should be visible
+    expect(menu.classList.contains("translate-x-full")).toBe(false);
+  });
+});
